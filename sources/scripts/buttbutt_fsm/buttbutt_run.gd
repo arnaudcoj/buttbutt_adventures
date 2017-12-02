4f4afaa3710ad7cf06bf16fa775a3c44c57c56af
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2017-12-02 12:32:17 +0100

    when walk and run are pressed run has priority

--- sources/scripts/buttbutt_fsm/buttbutt_run.gd
+++ sources/scripts/buttbutt_fsm/buttbutt_run.gd
@@ -14,9 +14,10 @@ func change_state():
 	elif fsm.is_control_pressed(fsm.Control.Up) and fsm.body.can_climb(): #TODO Jump
 		fsm.change_state(get_node(climbing_state_path))
 	elif not fsm.is_control_pressed(fsm.Control.RunLeft) and not fsm.is_control_pressed(fsm.Control.RunRight):
-		fsm.change_state(get_node(stopping_state_path))
-	elif fsm.is_control_pressed(fsm.Control.Left) or fsm.is_control_pressed(fsm.Control.Right):
-		fsm.change_state(get_node(walking_state_path))
+		if fsm.is_control_pressed(fsm.Control.Left) or fsm.is_control_pressed(fsm.Control.Right):
+			fsm.change_state(get_node(walking_state_path))
+		else:
+			fsm.change_state(get_node(stopping_state_path))
 
 func is_moving_right():
 	return fsm.is_control_pressed(fsm.Control.RunRight)
