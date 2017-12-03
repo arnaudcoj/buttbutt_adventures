d62cc2e532db46900d75cddd827dfe95a5c143bf
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2017-12-03 13:20:43 +0100

    fix falling gravity and inertia

--- sources/scripts/buttbutt_fsm/buttbutt_air_move.gd
+++ sources/scripts/buttbutt_fsm/buttbutt_air_move.gd
@@ -4,40 +4,42 @@ export (NodePath) var fsm_path
 var fsm
 
 export var speed = 500
+export var gravity_speed = 800
 export var gravity_direction = Vector2(0,1)
+export var gravity_acceleration = 0.05
 
 var inertia_velocity = Vector2()
 var gravity_velocity = Vector2()
-var player_velocity = Vector2()
 
 func _ready():
 	fsm = get_node(fsm_path)
 
 func on_enter():
-	inertia_velocity = fsm.current_speed
 	gravity_velocity = Vector2()
-	player_velocity = Vector2()
 
 func update(delta):
-	
-	inertia_velocity.x = lerp(inertia_velocity.x, 0, .1)
-	
+	inertia_velocity = fsm.current_speed
 	if not (fsm.is_control_pressed(fsm.Control.RunLeft) and fsm.is_control_pressed(fsm.Control.RunRight)) and not (fsm.is_control_pressed(fsm.Control.Left) and fsm.is_control_pressed(fsm.Control.Right)):
 		if fsm.is_control_pressed(fsm.Control.RunLeft):
-			player_velocity.x = lerp(player_velocity.x, -speed, .1)
+			inertia_velocity.x = lerp(inertia_velocity.x, -speed, .1)
 		elif fsm.is_control_pressed(fsm.Control.RunRight):
-			player_velocity.x = lerp(player_velocity.x, speed, .1)
+			inertia_velocity.x = lerp(inertia_velocity.x, speed, .1)
 		elif fsm.is_control_pressed(fsm.Control.Left):
-			player_velocity.x = lerp(player_velocity.x, -speed, .05)
+			inertia_velocity.x = lerp(inertia_velocity.x, -speed, .05)
 		elif fsm.is_control_pressed(fsm.Control.Right):
-			player_velocity.x = lerp(player_velocity.x, speed, .05)
+			inertia_velocity.x = lerp(inertia_velocity.x, speed, .05)
+		else:
+			inertia_velocity.x = lerp(inertia_velocity.x, 0, .01)
+	else:
+		inertia_velocity.x = lerp(inertia_velocity.x, 0, .01)
 
-	gravity_velocity.y = lerp(gravity_velocity.y, gravity_direction.y * speed, .2)
+	gravity_velocity.y = lerp(gravity_velocity.y, gravity_direction.y * gravity_speed, gravity_acceleration)
 
-	var motion = Vector2(inertia_velocity.x + player_velocity.x, gravity_velocity.y)
+	var motion = Vector2(inertia_velocity.x, gravity_velocity.y)
+	
 	motion.x = clamp(motion.x, -speed, speed)
-	motion.y = clamp(motion.y, -speed, speed)
-
+	motion.y = clamp(motion.y, -gravity_speed, gravity_speed)
+	
 	fsm.body.move_and_slide(motion)
 
 func on_leave():
