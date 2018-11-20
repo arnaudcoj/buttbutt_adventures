a0d749e44f9d75860572795c387807789f97935c
arnaudcoj <arnaudcoj@protonmail.com>
2018-11-20 23:17:27 +0100

    add debug game over

--- sources/entities/buttbutt/buttbutt.gd
+++ sources/entities/buttbutt/buttbutt.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal dead
+
 func get_left_ground_normal() :
 	if $LeftGroundRaycast.get_collider() != null:
 		return $LeftGroundRaycast.get_collision_normal()
@@ -9,3 +11,8 @@ func get_right_ground_normal() :
 	if $RightGroundRaycast.get_collider() != null:
 		return $RightGroundRaycast.get_collision_normal()
 	return null
+	
+func _on_area_entered(area):
+	emit_signal("dead")
+	#DEBUG
+	get_tree().reload_current_scene()
