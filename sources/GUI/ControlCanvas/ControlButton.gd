b2305a6c0bff13eb158d68e6e0796858b1014504
arnaudcoj <arnaudcoj@protonmail.com>
2018-11-20 21:52:42 +0100

    Reimplement multitouch controls

--- sources/GUI/ControlCanvas/ControlButton.gd
+++ sources/GUI/ControlCanvas/ControlButton.gd
@@ -2,8 +2,19 @@ extends Control
 
 export var action := "ui_select"
 
-func _on_button_pressed():
-	Input.action_press(action)
+var touch_id = null
 
-func _on_button_released():
-	Input.action_release(action)
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed:
+			if touch_id == null and is_inside(event.position):
+				touch_id = event.index
+				Input.action_press(action)
+		else:
+			if event.index == touch_id:
+				touch_id = null
+				Input.action_release(action)
+			
+func is_inside(position : Vector2) -> bool:
+	return (rect_position.x <= position.x and position.x <= rect_position.x + rect_size.x 
+	and rect_position.y <= position.y and position.y <= rect_position.y + rect_size.y )
