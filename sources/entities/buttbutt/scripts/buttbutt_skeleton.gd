44c97544b50ac085610182e8018df771099b3258
Non0w <arnaud.cojez@etudiant.univ-lille1.fr>
2019-07-20 20:08:45 +0200

    add draft flip code

--- sources/entities/buttbutt/scripts/buttbutt_skeleton.gd
+++ sources/entities/buttbutt/scripts/buttbutt_skeleton.gd
@@ -1,3 +1,11 @@
 extends Node2D
 
+class_name ButtButtSkeleton
+
 onready var animation_player = $AnimationPlayer
+
+const ORIENTATION_LEFT = 1
+const ORIENTATION_RIGHT = -1
+
+func flip(orientation):
+	scale.x = orientation
