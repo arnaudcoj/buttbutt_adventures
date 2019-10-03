cb90da9e1d646ad36b427e61b10a43df9070cc5d
Non0w <arnaudcoj@protonmail.com>
2019-10-03 23:31:44 +0200

    better animations and draft animationtree

--- sources/entities/buttbutt/scripts/buttbutt_skeleton.gd
+++ sources/entities/buttbutt/scripts/buttbutt_skeleton.gd
@@ -3,6 +3,7 @@ extends Node2D
 class_name ButtButtSkeleton
 
 onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
 
 const ORIENTATION_LEFT = 1
 const ORIENTATION_RIGHT = -1
